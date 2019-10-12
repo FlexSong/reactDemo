1ccf898dbe1722692103caa1c0a8bb024e469512
SongGang <15926387@qq.com>
2019-10-12 11:03:36 +0800

    第一次提交reactDemo

--- demo/03/src/App.jsx
+++ demo/03/src/App.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React,{Component} from 'react'
+import style from './App.less'
+class App extends Component{
+  constructor(){
+    super()
+    this.state={
+      name:"S&Z",
+      content:''
+    }
+  }
+  clickChange(){
+    this.setState({
+      name:"xixi"
+    })
+    this.test.style.color='blue'
+  }
+  changeInput(e){
+    this.setState({
+      content:e.target.value,
+    })
+  }
+  render(){
+    return(
+      <div>
+        <h2 style={{color:'#CC6666'}} ref={dom =>this.test =dom}>{this.state.name}</h2>
+        <button onClick={this.clickChange.bind(this)}>点击</button> <br/>
 
-const App = () => {
-  return <div>Hello React ...</div>
+        <input type='text' onChange={this.changeInput.bind(this)}></input>
+        <p className={style.firstP}>{this.state.content?this.state.content:"请输入内容"}</p>
+        
+      </div>
+    )
+  }
 }
 
 export default App
